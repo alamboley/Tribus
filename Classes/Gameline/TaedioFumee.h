9c10a8c887cb0d61b6e53004235dc0d6521973b9
Aymeric <aymeric.lamboley@gmail.com>
2012-05-30 01:19:08 +0200

    taedioFumee classes added

---
+++ Classes/Gameline/TaedioFumee.h
@@ -0,0 +1,13 @@
+//
+//  TaedioFumee.h
+//  ChipmunkWrapper
+//
+//  Created by Aymeric Lamboley on 29/05/12.
+//  Copyright (c) 2012 Sodeso. All rights reserved.
+//
+
+#import "Sensor.h"
+
+@interface TaedioFumee : Sensor
+
+@end
---
+++ Classes/Gameline/TaedioFumee.m
@@ -0,0 +1,13 @@
+//
+//  TaedioFumee.m
+//  ChipmunkWrapper
+//
+//  Created by Aymeric Lamboley on 29/05/12.
+//  Copyright (c) 2012 Sodeso. All rights reserved.
+//
+
+#import "TaedioFumee.h"
+
+@implementation TaedioFumee
+
+@end
