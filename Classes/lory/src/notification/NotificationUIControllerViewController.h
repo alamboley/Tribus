fd03c966931cf187d8e7b78e2b4989b8c404b354
lbineau <Mac@pc-crm-e01.gobelins-annecy.local>
2012-05-30 16:07:13 +0200

    Notification UIView created

---
+++ Classes/lory/src/notification/NotificationUIControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationUIControllerViewController.h
+//  ChipmunkWrapper
+//
+//  Created by Mac on 30/05/12.
+//  Copyright (c) 2012 Sodeso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationUIControllerViewController : UIViewController
+
+@end
