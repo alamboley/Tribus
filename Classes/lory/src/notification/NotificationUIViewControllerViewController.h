7063f20086d8bb15bd56630e289afd56f694638e
lbineau <Mac@pc2.home>
2012-06-03 13:12:47 +0200

    addNotificationView

---
+++ Classes/lory/src/notification/NotificationUIViewControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationUIViewControllerViewController.h
+//  ChipmunkWrapper
+//
+//  Created by Mac on 01/06/12.
+//  Copyright (c) 2012 Sodeso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationUIViewControllerViewController : UIViewController
+
+@end
