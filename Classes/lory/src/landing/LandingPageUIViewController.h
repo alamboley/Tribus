2dd205f1c772c2f0712ac7d4c5dcba4e132ffc1b
lbineau <Mac@MacBook-Pro-de-Mac.local>
2012-06-14 22:45:20 +0200

    create landing page

---
+++ Classes/lory/src/landing/LandingPageUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandingPageUIViewController.h
+//  ChipmunkWrapper
+//
+//  Created by Mac on 14/06/12.
+//  Copyright (c) 2012 Sodeso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingPageUIViewController : UIViewController
+
+@end
